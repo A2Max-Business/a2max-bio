45d82026b16b7e8f4914176ebb86745296edfd43
Gigante Pneus <mktgigantepneus@gmail.com>
2026-09-18 22:53:21 -0300

    style: reduce distance between hero text and conceptual bar

--- components/brand/HeroSection.tsx
+++ components/brand/HeroSection.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export function HeroSection() {
   return (
-    <div className="relative w-full flex flex-col overflow-hidden pt-[92px] pb-4 animate-hero-bg min-h-[350px]">
+    <div className="relative w-full flex flex-col overflow-hidden pt-[92px] pb-0 animate-hero-bg min-h-[350px]">
       {/* Background Image with Object Fit */}
       <div className="absolute inset-0 z-0">
         <Image
